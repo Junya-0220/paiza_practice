54cb0f67efdb0edd7c9d35cba220f04d1c54114e
Junya-0220 <54745459+Junya-0220@users.noreply.github.com>
2023-07-08 16:18:08 +0900

    commit

--- Arankup/maphantei.go
+++ Arankup/maphantei.go
@@ -1,6 +1,12 @@
 package arankup
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func ReplaceCharAt(str string, index int, newChar byte) string {
 	strBytes := []byte(str)
@@ -32,5 +38,37 @@ func JudgeYoko(H, W int) {
 				}
 			}
 		}
+	}
+}
+
+func NijigenHairetsu {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	// H, W, N の入力を受け取る
+	scanner.Scan()
+	line := scanner.Text()
+	values := strings.Split(line, " ")
+	H, _ := strconv.Atoi(values[0])
+	_, _ = strconv.Atoi(values[1]) // Wは使用しないため、読み飛ばす
+	N, _ := strconv.Atoi(values[2])
+
+	// S を入力して二次元スライスに格納する
+	S := make([][]byte, H)
+	for i := 0; i < H; i++ {
+		scanner.Scan()
+		S[i] = []byte(scanner.Text())
+	}
+
+	// N 回のクエリを処理する
+	for i := 0; i < N; i++ {
+		// y, x の入力を受け取る
+		scanner.Scan()
+		line := scanner.Text()
+		values := strings.Split(line, " ")
+		y, _ := strconv.Atoi(values[0])
+		x, _ := strconv.Atoi(values[1])
+
+		// S[y][x] の値を出力する
+		fmt.Println(string(S[y][x]))
 	}
 }
