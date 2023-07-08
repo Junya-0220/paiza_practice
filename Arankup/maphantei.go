5be74f49ff4db361f1292b45cb336275a3f5051f
Junya-0220 <junya010220@gmail.com>
2023-07-08 21:43:56 +0900

    修正

--- Arankup/maphantei.go
+++ Arankup/maphantei.go
@@ -1,11 +1,7 @@
 package arankup
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strconv"
-	"strings"
 )
 
 func ReplaceCharAt(str string, index int, newChar byte) string {
@@ -36,109 +32,6 @@ func JudgeYoko(H, W int) {
 				if j == W-1 || S[i][j+1] == '#' {
 					fmt.Println(i, j)
 				}
-			}
-		}
-	}
-}
-
-func NijigenHairetsu {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	// H, W, N の入力を受け取る
-	scanner.Scan()
-	line := scanner.Text()
-	values := strings.Split(line, " ")
-	H, _ := strconv.Atoi(values[0])
-	_, _ = strconv.Atoi(values[1]) // Wは使用しないため、読み飛ばす
-	N, _ := strconv.Atoi(values[2])
-
-	// S を入力して二次元スライスに格納する
-	S := make([][]byte, H)
-	for i := 0; i < H; i++ {
-		scanner.Scan()
-		S[i] = []byte(scanner.Text())
-	}
-
-	// N 回のクエリを処理する
-	for i := 0; i < N; i++ {
-		// y, x の入力を受け取る
-		scanner.Scan()
-		line := scanner.Text()
-		values := strings.Split(line, " ")
-		y, _ := strconv.Atoi(values[0])
-		x, _ := strconv.Atoi(values[1])
-
-		// S[y][x] の値を出力する
-		fmt.Println(string(S[y][x]))
-	}
-}
-
-func NijigenHairetsu2 {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	// h, w の入力を受け取る
-	scanner.Scan()
-	line := scanner.Text()
-	values := strings.Split(line, " ")
-	h, _ := strconv.Atoi(values[0])
-	w, _ := strconv.Atoi(values[1])
-
-	// s を入力して二次元スライスに格納する
-	s := make([][]byte, h)
-	for i := 0; i < h; i++ {
-		scanner.Scan()
-		s[i] = []byte(scanner.Text())
-	}
-
-	// 条件に合致する座標を出力する
-	for y := 0; y < h; y++ {
-		for x := 0; x < w; x++ {
-			if x == 0 || s[y][x-1] == '#' {
-				if x == w-1 || s[y][x+1] == '#' {
-					fmt.Println(y, x)
-				}
-			}
-		}
-	}
-}
-
-func ChalengeNijigenHairetsu {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	// h, w の入力を受け取る
-	scanner.Scan()
-	line := scanner.Text()
-	values := strings.Split(line, " ")
-	h, _ := strconv.Atoi(values[0])
-	w, _ := strconv.Atoi(values[1])
-
-	// s を入力して二次元スライスに格納する
-	s := make([][]byte, h)
-	for i := 0; i < h; i++ {
-		scanner.Scan()
-		s[i] = []byte(scanner.Text())
-	}
-
-	// 条件に合致する座標を出力する
-	for y := 0; y < h; y++ {
-		for x := 0; x < w; x++ {
-			flagRow := false
-			flagColumn := false
-
-			if x == 0 || s[y][x-1] == '#' {
-				if x == w-1 || s[y][x+1] == '#' {
-					flagRow = true
-				}
-			}
-
-			if y == 0 || s[y-1][x] == '#' {
-				if y == h-1 || s[y+1][x] == '#' {
-					flagColumn = true
-				}
-			}
-
-			if flagRow && flagColumn {
-				fmt.Println(y, x)
 			}
 		}
 	}
--- main.go
+++ main.go
@@ -1,12 +1,4 @@
 package main
 
-import (
-	"fmt"
-	arankup "paiza_pracitce/Arankup"
-)
-
 func main() {
-	var H, W int
-	fmt.Scan(&H, &W)
-	arankup.JudgeYoko(H, W)
 }
