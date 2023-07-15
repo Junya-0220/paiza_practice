69738dcf175e609d99e6773fba1646a27e46cded
Junya-0220 <junya010220@gmail.com>
2023-07-15 13:18:34 +0900

    binarySearchのアルゴリズム

--- algo/search/searchpractice.go
+++ algo/search/searchpractice.go
@@ -2,24 +2,47 @@ package searchpractice
 
 type IndexNum int
 
-func linearSearch(numbers []int, value int) IndexNum {
+/*
+nums := []int{0, 1, 5, 7, 9, 11, 15, 20, 24}
+s := searchpractice.LinearSearch(nums, 15)
+*/
+func LinearSearch(numbers []int, value int) IndexNum {
+	//見つかった時のIndex番号を返す
 	for i := 0; i < len(numbers); i++ {
 		if numbers[i] == value {
 			return IndexNum(i)
 		}
 	}
+	//見つからない場合
 	return -1
 }
 
+/*
+nums := []int{0, 1, 5, 7, 9, 11, 15, 20, 24}
+s := searchpractice.BinarySearch(nums, 15)
+*/
 func BinarySearch(numbers []int, value int) IndexNum {
+	//leftは0,rightは配列の末尾を初期値にする
 	left, right := 0, len(numbers)-1
+	//leftがrightよりも大きくなるまでループをする
 	for left <= right {
 		mid := (left + right) / 2
 		if numbers[mid] == value {
+			//半分の地点で探したい値が見つかったらそれを返す
 			return IndexNum(mid)
 		} else if numbers[mid] < value {
+			/*
+				探したい値が見つからなかった時、numbsers[mid]と探したい値を比較して、
+				numbsers[mid]よりも探したい値の値が大きいなら、
+				leftにmid + 1をする
+			*/
 			left = mid + 1
 		} else {
+			/*
+				探したい値が見つからなかった時、numbsers[mid]と探したい値を比較して、
+				numbsers[mid]よりも探したい値の値が小さいなら、
+				rightにmid - 1をする
+			*/
 			right = mid - 1
 		}
 	}
--- main.go
+++ main.go
@@ -1,36 +1,19 @@
 package main
 
-import "fmt"
-
-type Node struct {
-	value int
-	left  *Node
-	right *Node
-}
-
-func insert(node *Node, value int) *Node {
-	if node == nil {
-		return &Node{value: value}
-	}
-
-	if value < node.value {
-		node.left = insert(node.left, value)
-	} else {
-		node.right = insert(node.right, value)
-	}
-	return node
-}
+import (
+	"fmt"
+	searchpractice "paiza_pracitce/algo/search"
+)
 
 func main() {
-	var root *Node
-	root = insert(root, 3)
-	root = insert(root, 6)
-	root = insert(root, 5)
-	root = insert(root, 7)
-	root = insert(root, 1)
-	root = insert(root, 10)
-	root = insert(root, 2)
-	root = insert(root, 8)
-	root = insert(root, 700)
-	fmt.Println(root.left.value, root.right.value)
+	nums := []int{0, 1, 5, 7, 9, 11, 15, 20, 24}
+	fmt.Println("numsの長さ", len(nums)-1)
+	target := 24
+	s := searchpractice.BinarySearch(nums, target)
+	fmt.Println("index番号は:", s)
+	if s == -1 {
+		fmt.Println("見つからなかった")
+	} else {
+		fmt.Println(nums[s])
+	}
 }
